24ddbf34a7df7e919948af8596e4e7f8933a98be
Nathaniel Lewis <linux.robotdude@gmail.com>
2014-10-06 15:34:16 -0700

    Implemented cache writes, reorganized some code

--- project1/cachelogic.c
+++ project1/cachelogic.c
@@ -67,6 +67,55 @@ void init_lru(int assoc_index, int block_index)
 }
 
 /*
+  Return the block to replace
+*/
+
+unsigned int blockToReplace(unsigned int set)
+{
+    // If we failed to retrieve from cache, attempt to load it
+    // Attempt to find an invalid spot
+    for(unsigned int i = 0; i < assoc; i++)
+    {
+        if(cache[set].block[i].valid == INVALID)
+        {
+            return i;
+        }
+    }
+
+    // If there were no invalid blocks, implement our replacement policy
+    // random replacement
+    if(policy == RANDOM)
+    {
+        return randomint(assoc);
+    }
+
+    // least frequently used
+    else if (policy == LFU)
+    {
+        unsigned int lowest = 0xFFFFFFFF;
+        unsigned int block = 0;
+        for(int i = 0; i < assoc; i++)
+        {
+            if(cache[set].block[i].accessCount < lowest)
+            {
+                block = i;
+                lowest = cache[set].block[i].accessCount;
+            }
+        }
+        return block;
+    }
+
+    // least recently used
+    else
+    {
+        // IMPLEMENT
+        assert(0);
+    }
+
+    return 0xFFFFFFFF;
+}
+
+/*
   This is the primary function you are filling out,
   You are free to add helper functions if you need them
 
@@ -80,151 +129,121 @@ void init_lru(int assoc_index, int block_index)
 */
 void accessMemory(address addr, word* data, WriteEnable we)
 {
-  /* Declare variables here */
+    /* handle the case of no cache at all - leave this in */
+    if(assoc == 0 || view != ASSOC)
+    {
+        accessDRAM(addr, (byte*)data, WORD_SIZE, we);
+        return;
+    }
 
-  /* handle the case of no cache at all - leave this in */
-  if(assoc == 0 || view != ASSOC) 
-  {
-    accessDRAM(addr, (byte*)data, WORD_SIZE, we);
-    return;
-  }
+    // TODO - handle edge case where block size and/or set count are 1
 
-  // Deal with an associative cache 
-  if(view == ASSOC)
-  {
     unsigned int oBits = uint_log2(block_size);
     unsigned int sBits = uint_log2(set_count);
     unsigned int tBits = 32 - (oBits + sBits);
-    
+
     unsigned int oMask = (1 << oBits) - 1;
     unsigned int sMask = (1 << sBits) - 1;
     unsigned int tMask = (1 << tBits) - 1;
-    
+
     unsigned int offset = addr & oMask;
     unsigned int set = (addr >> oBits) & sMask;
     unsigned int tag = (addr >> (oBits + sBits)) & tMask;
-    
-    // If we are performing a memory read
-    if(we == READ)
+
+    // Deal with an associative cache
+    if(view == ASSOC)
     {
-      // Assoc block to access
-      unsigned int block = 0;
-    
-      // Attempt to find the row in cache
-      for(unsigned int i = 0; i < assoc; i++)
-      {
-        if(cache[set].block[i].tag == tag)
+        // Assoc block to access
+        unsigned int block = 0;
+
+        // Attempt to find the row in cache
+        for(unsigned int i = 0; i < assoc; i++)
         {
-          block = i;
-          if(cache[set].block[i].valid == INVALID)
-          {
-            goto load;
-          }
-        
-          // CACHE HIT!
-          goto done;
+            if(cache[set].block[i].tag == tag)
+            {
+                block = i;
+                if(cache[set].block[i].valid == INVALID)
+                {
+                    // compulsory miss
+                    goto load;
+                }
+
+                // hit
+                goto service;
+            }
         }
-      }
-      
-      // If we failed to retrieve from cache, attempt to load it
-      // Attempt to find an invalid spot
-      for(unsigned int i = 0; i < assoc; i++)
-      {
-        if(cache[set].block[i].valid == INVALID)
-        {
-          block = i;
-          goto load; 
-        }
-      }
-      
-      // If there were no invalid blocks, implement our replacement policy
-      // random replacement
-      if(policy == RANDOM)
-      {
-        block = randomint(assoc);
-      }
-      
-      // least frequently used
-      else if (policy == LFU)
-      {
-        unsigned int lowest = 0xFFFFFFFF;
-        for(int i = 0; i < assoc; i++)
-        {
-          if(cache[set].block[i].accessCount < lowest)
-          {
-            block = i;
-            lowest = cache[set].block[i].accessCount;
-          }
-        }
-      }
-      
-      // least recently used
-      else
-      {
-        // IMPLEMENT
-        assert(0);
-      }
+
+        // miss
+        block = blockToReplace(set);
 
 load:
-      // If the block we are about to replace is dirty, replace it in memory
-      if(cache[set].block[block].dirty == DIRTY)
-      {
-          // Compute the address
-          address a = cache[set].block[block].tag << (oBits + sBits);
-          a |= (set << oBits);
-          for(int j = 0; j < block_size; j++)
-              accessDRAM(a + j, (byte *) cache[set].block[block].data + j, BYTE_SIZE, WRITE);
-      }
+        // If the block we are about to replace is dirty, replace it in memory
+        if(cache[set].block[block].dirty == DIRTY && cache[set].block[block].valid == VALID)
+        {
+            // Compute the address
+            address a = cache[set].block[block].tag << (oBits + sBits);
+            a |= (set << oBits);
+            for(int j = 0; j < block_size; j++)
+            accessDRAM(a + j, (byte *) cache[set].block[block].data + j, BYTE_SIZE, WRITE);
+        }
 
-      for(int j = 0; j < block_size; j++)
-        accessDRAM(addr + j, (byte *) cache[set].block[block].data + j, BYTE_SIZE, READ);
-      cache[set].block[block].valid = VALID;
-      cache[set].block[block].dirty = VIRGIN;
-      cache[set].block[block].tag = tag;
-      cache[set].block[block].accessCount = 0;
-      
-done:
-      // Service the read
-      memcpy((void *) data, (void *) ((byte *) cache[set].block[block].data + offset), sizeof(word));
-      cache[set].block[block].accessCount++;
+        // Perform load from dram
+        for(int j = 0; j < block_size; j++)
+            accessDRAM(addr + j, (byte *) cache[set].block[block].data + j, BYTE_SIZE, READ);
+        cache[set].block[block].valid = VALID;
+        cache[set].block[block].dirty = VIRGIN;
+        cache[set].block[block].tag = tag;
+        cache[set].block[block].accessCount = 0;
+
+service:
+        // If we are reading from the memory
+        if(we == READ)
+        {
+            // Service the read
+            memcpy((void *) data, (void *) ((byte *) cache[set].block[block].data + offset), sizeof(word));
+            cache[set].block[block].accessCount++;
+        }
+
+        // Otherwise we are writing to memory
+        else
+        {
+            // Service the write
+            memcpy((void *) ((byte *) cache[set].block[block].data + offset), (void *) data, sizeof(word));
+            cache[set].block[block].accessCount++;
+
+            // If the replacement policy is write through, service dram
+            if(memory_sync_policy == WRITE_THROUGH)
+            {
+                for(int j = 0; j < block_size; j++)
+                    accessDRAM(addr + j, (byte *) cache[set].block[block].data + j, BYTE_SIZE, WRITE);
+            }
+
+            // Otherwise we have dirty memory
+            else
+                cache[set].block[block].dirty = DIRTY;
+        }
     }
-  }
-
-  /*
-  You need to read/write between memory (via the accessDRAM() function) and
-  the cache (via the cache[] global structure defined in tips.h)
-
-  Remember to read tips.h for all the global variables that tell you the
-  cache parameters
-
-  The same code should handle random, LFU, and LRU policies. Test the policy
-  variable (see tips.h) to decide which policy to execute. The LRU policy
-  should be written such that no two blocks (when their valid bit is VALID)
-  will ever be a candidate for replacement. In the case of a tie in the
-  least number of accesses for LFU, you use the LRU information to determine
-  which block to replace.
-
-  Your cache should be able to support write-through mode (any writes to
-  the cache get immediately copied to main memory also) and write-back mode
-  (and writes to the cache only gets copied to main memory when the block
-  is kicked out of the cache.
-
-  Also, cache should do allocate-on-write. This means, a write operation
-  will bring in an entire block if the block is not already in the cache.
-
-  To properly work with the GUI, the code needs to tell the GUI code
-  when to redraw and when to flash things. Descriptions of the animation
-  functions can be found in tips.h
-  */
-
-  /* Start adding code here */
-
-
-  /* This call to accessDRAM occurs when you modify any of the
-     cache parameters. It is provided as a stop gap solution.
-     At some point, ONCE YOU HAVE MORE OF YOUR CACHELOGIC IN PLACE,
-     THIS LINE SHOULD BE REMOVED.
-  */
-  
-  //accessDRAM(addr, (byte*)data, WORD_SIZE, we);
 }
+
+
+/*
+The same code should handle random, LFU, and LRU policies. Test the policy
+variable (see tips.h) to decide which policy to execute. The LRU policy
+should be written such that no two blocks (when their valid bit is VALID)
+will ever be a candidate for replacement. In the case of a tie in the
+least number of accesses for LFU, you use the LRU information to determine
+which block to replace.
+
+Your cache should be able to support write-through mode (any writes to
+the cache get immediately copied to main memory also) and write-back mode
+(and writes to the cache only gets copied to main memory when the block
+is kicked out of the cache.
+
+Also, cache should do allocate-on-write. This means, a write operation
+will bring in an entire block if the block is not already in the cache.
+
+To properly work with the GUI, the code needs to tell the GUI code
+when to redraw and when to flash things. Descriptions of the animation
+functions can be found in tips.h
+*/
+
