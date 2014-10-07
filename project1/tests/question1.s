0153145316cb9eebc15d9873e1cb46114554b832
Nathaniel Lewis <linux.robotdude@gmail.com>
2014-10-07 16:09:24 -0700

    Added useful comments in question1.s

--- project1/tests/question1.s
+++ project1/tests/question1.s
@@ -1,6 +1,9 @@
-xor $s0, $s0, $s0    # total = 0
-xor $s1, $s1, $s1    # array = 0
-xor $s2, $s2, $s2    # x = 0
+xor $s0, $s0, $s0    # int total = 0
+xor $s1, $s1, $s1    # char* array = 0x00000000
+
+# for(int x = 0; x < 64; x++) array[x] = x
+_lp1_begin:
+    xor $s2, $s2, $s2    # x = 0
 
 _lp1:
     # loop logic
@@ -13,6 +16,8 @@ _lp1:
     addiu $s2, $s2, 1
     j _lp1
 
+
+# for(int x = 0; x < 64; x++) total += array[x]
 _lp2_begin:
     xor $s2, $s2, $s2    # x = 0
 
@@ -28,6 +33,7 @@ _lp2:
     addiu $s2, $s2, 1
     j _lp2
 
+# for(int x = 0; x < 64; x++) total += array[x]
 _lp3_begin:
     xor $s2, $s2, $s2    # x = 0
 
