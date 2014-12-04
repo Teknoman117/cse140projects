fe4a82e0fcbc168822ad5f752ef1af0d6f7062b3
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2014-12-03 23:43:07 -0800

    Updated project2 skeleton code to latest version as provided by Kelvin.

--- project2/benchmark.c
+++ project2/benchmark.c
@@ -9,22 +9,15 @@
 
 /* Your function must have the following signature: */
 
-void sgemm( int m, int n, int d, float *A, float *C );
-
+void sgemm( int m, int n, float *A, float *C );
 
 /* The reference code */
 void sgemm_reference( int m, int n, float *A, float *C )
 {
-  /*#pragma omp parallel for
-  for( int i = 0; i < n; i++ )
-    for( int k = 0; k < m; k++ )
-      for( int j = 0; j < n; j++ )
-	C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];*/
-  #pragma omp parallel for
-  for( int i = 0; i < n; i++)
-    for( int k = 0; k < m; k++)
-      for( int j = 0; j < n; j++)
-        C[j+(i*n)] += A[j+(k*n)] * A[i+(k*n)];
+  for( int i = 0; i < m; i++ )
+    for( int k = 0; k < n; k++ ) 
+      for( int j = 0; j < m; j++ ) 
+        C[i+j*m] += A[i+k*m] * A[j+k*m];
 }
 
 /* The benchmarking program */
@@ -41,51 +34,52 @@ int main( int argc, char **argv )
 //for( int m = 48; m <  49; m = m+1+m/3 )
   {
     /* Allocate and fill 2 random matrices A, C */
-    float *A = (float*) malloc( (n+m) * n * sizeof(float) );
-    float *C = (float*) malloc( n * n * sizeof(float) );
-    float *C_ref = (float*) malloc( n * n * sizeof(float) );
-
-    for( int i = 0; i < (n+m)*n; i++ ) A[i] = 2 * drand48() - 1;
-
+    float *A = (float*) malloc( m * n * sizeof(float) );
+    float *C = (float*) malloc( m * m * sizeof(float) );
+    float *C_ref = (float*) malloc( m * m * sizeof(float) );
+    
+    for( int i = 0; i < m*n; i++ ) A[i] = 2 * drand48() - 1;
+    for( int i = 0; i < m*m; i++ ) C[i] = 2 * drand48() - 1;
+ 
     /* measure Gflop/s rate; time a sufficiently long sequence of calls to eliminate noise */
     double Gflop_s, seconds = -1.0;
-    for( int n_iterations = 1; seconds < 0.1; n_iterations *= 2 )
+    for( int n_iterations = 1; seconds < 0.1; n_iterations *= 2 ) 
     {
       /* warm-up */
-      sgemm( m, n,m, A, C );
-
+      sgemm( m, n, A, C );
+      
       /* measure time */
       struct timeval start, end;
       gettimeofday( &start, NULL );
       for( int i = 0; i < n_iterations; i++ )
-	sgemm( m,n,m, A, C );
+	sgemm( m,n,A, C );
       gettimeofday( &end, NULL );
       seconds = (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
-
+      
       /* compute Gflop/s rate */
-      Gflop_s = 2e-9 * n_iterations * m * n * n / seconds;
+      Gflop_s = 2e-9 * n_iterations * m * m * n / seconds;
     }
-
+    
     printf( "%d by %d matrix with strip size %d \t %g Gflop/s\n", n, n,m, Gflop_s );
-
+    
     /* Ensure that error does not exceed the theoretical error bound */
-
+		
     /* Set initial C to 0 and do matrix multiply of A*B */
-    memset( C, 0, sizeof( float ) * n * n );
-    sgemm( m,n,m, A, C );
+    memset( C, 0, sizeof( float ) * m * m );
+    sgemm( m,n, A, C );
 
     /* Subtract A*B from C using standard sgemm and reference (note that this should be 0 to within machine roundoff) */
-    memset( C_ref, 0, sizeof( float ) * n * n );
+    memset( C_ref, 0, sizeof( float ) * m * m );
     sgemm_reference( m,n,A,C_ref );
 
     /* Subtract the maximum allowed roundoff from each element of C */
-    for( int i = 0; i < n*n; i++ ) C[i] -= C_ref[i] ;
+    for( int i = 0; i < m*m; i++ ) C[i] -= C_ref[i] ; 
 
     /* After this test if any element in C is still positive something went wrong in square_sgemm */
-    for( int i = 0; i < n * n; i++ )
-      if( C[i] > 0.0001 ) {
+    for( int i = 0; i < m * m; i++ )
+      if( C[i] > 0 ) {
   	printf( "FAILURE: error in matrix multiply exceeds an acceptable margin\n" );
-  	printf( "Off by: %f, from the reference: %f, at n = %d, m = %d\n",C[i], C_ref[i], n, m );
+  	printf( "Off by: %f, from the reference: %f, at n = %d, m = %d\n",C[i], C_ref[i], n, m ); 
   	return -1;
       }
 
@@ -94,6 +88,6 @@ int main( int argc, char **argv )
     free( C );
     free( A );
   }
-	}
+	}  
   return 0;
 }
--- project2/sgemm-naive.c
+++ project2/sgemm-naive.c
@@ -1,21 +1,7 @@
-void sgemm( int m, int n, int d, float *A, float *C )
+void sgemm( int m, int n, float *A, float *C )
 {
-    // this addresses columns of the destination matrix
-    /*for( int i = 0; i < n; i++ )
-    {
-        // this addresses col
-        for( int k = 0; k < m; k++ )
-        {
-            // this accesses rows of the destination matrix
-            for( int j = 0; j < n; j++ )
-            {
-                C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-            }
-        }
-    }*/
-    #pragma omp parallel for
-    for( int i = 0; i < n; i++)
-      for( int k = 0; k < m; k++)
-        for( int j = 0; j < n; j++)
-          C[j+(i*n)] += A[j+(k*n)] * A[i+(k*n)];
+  for( int i = 0; i < m; i++ )
+    for( int k = 0; k < n; k++ ) 
+      for( int j = 0; j < m; j++ ) 
+	C[i+j*m] += A[i+k*m] * A[j+k*m];
 }
