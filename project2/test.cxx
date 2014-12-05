2b7204116d417db56f6706fe67f736a88afe097b
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2014-12-04 22:30:37 -0800

    Cleanup and attempt change everything to row major (as side project)

--- project2/test.cxx
+++ project2/test.cxx
@@ -1,77 +1,136 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <float.h>
-#include <math.h>
-#include <time.h>
-#include <sys/time.h>
-#include <emmintrin.h>
+#include <iostream>
+#include <malloc.h>
+#include <cstring>
 
-/* Your function must have the following signature: */
+#include <xmmintrin.h>
 
-extern "C" void sgemm( int m, int n, int d, float *A, float *C );
-
-
-/* The reference code */
-void sgemm_reference( int m, int n, float *A, float *C )
+/* method provided by the code */
+void sgemm_reference( int m, int n, float *A, float *C)
 {
-  /*#pragma omp parallel for
-  for( int i = 0; i < n; i++ )
-    for( int k = 0; k < m; k++ )
-      for( int j = 0; j < n; j++ )
-        C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];*/
-
-  #pragma omp parallel for
-  for( int i = 0; i < n; i++)
-    for( int j = 0; j < n; j++)
-      for( int k = 0; k < m; k++)
-        C[j+(i*n)] += A[j+(k*n)] * A[i+(k*n)];
+    for(size_t i = 0; i < m; i++)
+        for(size_t j = 0; j < m; j++)
+            for(size_t k = 0; k < n; k++)
+                 C[(i*m)+j] += A[(j*n) + k] * A[(i*m) + k];
 }
 
-/* The benchmarking program */
-
-int main( int argc, char **argv )
+// Computes C = A * transpose(A).  m and n must be multiples of 4. A is row major, C is column major
+void atimestransposea( int m, int n, float *A, float *C )
 {
-  srand(time(NULL));
-
-  int n = 3;
-  int m = 2;
-
-  float matrix[] = {1.0f, 8.0f, 3.0f, 4.0f, 7.0f, 9.0f};
-
-  /* Allocate and fill 2 random matrices A, C */
-  float *A = static_cast<float *>(matrix);
-  float *C = (float*) malloc( n * n * sizeof(float) );
-  float *C_ref = (float*) malloc( n * n * sizeof(float) );
-
-  //for( int i = 0; i < (n+m)*n; i++ ) A[i] = 2 * drand48() - 1;
-
-  /* Ensure that error does not exceed the theoretical error bound */
-
-  /* Set initial C to 0 and do matrix multiply of A*B */
-  memset( C, 0, sizeof( float ) * n * n );
-  sgemm( m,n,m, A, C );
-
-  /* Subtract A*B from C using standard sgemm and reference (note that this should be 0 to within machine roundoff) */
-  memset( C_ref, 0, sizeof( float ) * n * n );
-  sgemm_reference( m,n,A,C_ref );
-
-  /* Subtract the maximum allowed roundoff from each element of C */
-  for( int i = 0; i < n*n; i++ ) C[i] -= C_ref[i] ;
-
-  /* After this test if any element in C is still positive something went wrong in square_sgemm */
-  for( int i = 0; i < n * n; i++ )
-    if( C[i] > 0.0001 )
+    // Iterate through the columns of the matrix
+    for(size_t i = 0; i < m; i++)
     {
-      printf( "FAILURE: error in matrix multiply exceeds an acceptable margin\n" );
-      printf( "Off by: %f, from the reference: %f, at n = %d, m = %d\n",C[i], C_ref[i], n, m );
-      return -1;
+        // Iterate through the rows of the matrix
+        for(size_t j = 0; j < m; j += 4)
+        {
+            // Summation register for these elements
+            __m128 result = _mm_set1_ps(0.0f);
+
+            // Iterate over the elements in a row
+            for(size_t k = 0; k < n; k += 4)
+            {
+                // Load r-matrix column
+                __m128 aColumn = _mm_load_ps(A + (i*n) + k);
+
+                // Load l-matrix rows
+                __m128 aRow0 = _mm_load_ps(A + ((j+0)*n) + k) * aColumn;
+                __m128 aRow1 = _mm_load_ps(A + ((j+1)*n) + k) * aColumn;
+                __m128 aRow2 = _mm_load_ps(A + ((j+2)*n) + k) * aColumn;
+                __m128 aRow3 = _mm_load_ps(A + ((j+3)*n) + k) * aColumn;
+
+                // Transpose the row so that we can use vector add
+                _MM_TRANSPOSE4_PS(aRow0, aRow1, aRow2, aRow3);
+
+                // Store the summations
+                result += aRow0;
+                result += aRow1;
+                result += aRow2;
+                result += aRow3;
+            }
+
+            // Store result
+            _mm_store_ps(C + (i*m) + j, result);
+        }
+    }
+}
+
+// Multiply A matrix by its transpose
+extern "C" void sgemm( int m, int n, float *A, float *C )
+{
+    // The matrix does not have favorable dimensions
+    if((m % 4) || (n % 4))
+    {
+        // Recompute boundaries of the matrix (align to 4, for sse)
+        int mPadded = (m & ~0x03) + ((m & 0x03) ? 4 : 0);
+        int nPadded = (n & ~0x03) + ((n & 0x03) ? 4 : 0);
+
+        // Allocate new, padded matrices
+        float *Apadded = (float *) malloc (sizeof(float) * mPadded * nPadded);
+        float *Cpadded = (float *) malloc (sizeof(float) * mPadded * mPadded);
+        memset((void *) Apadded, 0, sizeof(float) * mPadded * nPadded);
+
+        // Perform a copy of A into padded matrix (optimized for row major matrices)
+        for(int j = 0; j < m; j++)
+            memcpy((void *) (Apadded + (nPadded*j)), (void *) (A + (n*j)), sizeof(float) * n);
+
+        // Perform multiplication
+        atimestransposea(mPadded, nPadded, Apadded, Cpadded);
+
+        // Perform a copy of Cpadded into C matrix (optimized for column major matrices)
+        for(int j = 0; j < m; j++)
+            memcpy((void *) (C + (m*j)), (void *) (Cpadded + (mPadded*j)), sizeof(float) * m);
+
+        // Cleanup
+        free(Cpadded);
+        free(Apadded);
     }
 
-  /* release memory */
-  free( C_ref );
-  free( C );
-  //free( A );
+    // Otherwise, this is an optimal case where padding is not required
+    else
+    {
+        atimestransposea(m, n, A, C);
+    }
+}
+
+// Test
+int main ()
+{
+  size_t m = 8;
+  size_t n = 4;
+  float  matrixA[] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f,
+                      9.0f, 10.0f, 11.0f, 12.0f, 13.0f, 14.0f, 15.0f, 16.0f,
+                      1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f,
+                      9.0f, 10.0f, 11.0f, 12.0f, 13.0f, 14.0f, 15.0f, 16.0f};
+  float  matrixC[m*m];
+  float *A  = (float *) matrixA;
+  float *C  = (float *) matrixC;
+
+  // Print the source matrix
+  std::cout << "Computing C = A * transpose(A)" << std::endl;
+  std::cout << "A[]:\t";
+  for(size_t i = 0; i < m * n; i++)
+    std::cout << matrixA[i] << "\t";
+  std::cout << std::endl;
+
+  // Perform multiplication with the original method
+  memset((void *) C, 0, sizeof(float) * m * m);
+  sgemm_reference(m, n, A, C);
+  std::cout << "C[]:\t";
+  for(size_t i = 0; i < m * m; i++)
+    std::cout << C[i] << "\t";
+  std::cout << std::endl << std::endl;
+
+  // Zero the destination array
+  memset((void *) C, 0, sizeof(float) * m * m);
+
+  // Iterate through the columns of the matrix
+  sgemm(m,n,A,C);
+
+  // Output resulting arrays
+  std::cout << "Result using SSE optimized algorithm" << std::endl;
+  std::cout << "C[]:\t";
+  for(size_t i = 0; i < m * m; i++)
+    std::cout << C[i] << "\t";
+  std::cout << std::endl << std::endl;
 
   return 0;
 }
