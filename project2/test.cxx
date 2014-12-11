b9f25b3c8cc6b3d5b2f371e1d7049086d0e28202
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2014-12-11 00:37:19 -0800

    In progress matrix block multiplier. Source matrix slicing is complete.  Transpose slicer in progress

--- project2/test.cxx
+++ project2/test.cxx
@@ -1,170 +1,257 @@
 #include <iostream>
 #include <malloc.h>
 #include <cstring>
+#include <sstream>
 
 #include <xmmintrin.h>
+
+// Index into an MxN matrix.  r = row #, c = column #
+#define ROW_MAJOR(r,c,M,N)    ((r)*N)+(c)
+#define COLUMN_MAJOR(r,c,M,N) ((c)*M)+(r)
+#define MIN(a,b) (a<b)?a:b
+
+/* print row major matrix */
+void print_rowmajor(float *A, int m, int n, std::string name)
+{
+    std::cout << name;
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+            std::cout << "\t" << A[ROW_MAJOR(i,j,m,n)];
+        std::cout << std::endl;
+    }
+}
+
+/* print column major matrix */
+void print_columnmajor(float *A, int m, int n, std::string name)
+{
+    std::cout << name;
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+            std::cout << "\t" << A[COLUMN_MAJOR(i,j,m,n)];
+        std::cout << std::endl;
+    }
+}
+
+// Computes product of 2 4x4 matrices (C += A * B)
+//   A -> row major matrix
+//   B -> column major matrix
+//   C -> column major matrix
+void block_multiply_accumulate( float *A, float *B, float *C )
+{
+    // Intermediates for summation
+    __m128 intermediate0, intermediate1, intermediate2, intermediate3;
+
+    // Load all the rows of the left matrix
+    __m128 row0 = _mm_load_ps(A + 0);
+    __m128 row1 = _mm_load_ps(A + 4);
+    __m128 row2 = _mm_load_ps(A + 8);
+    __m128 row3 = _mm_load_ps(A + 12);
+
+    // Compute the sums for the first column
+    __m128 column0      = _mm_load_ps(B + 0);
+    __m128 destination0 = _mm_load_ps(C + 0);
+    intermediate0 = row0 * column0;
+    intermediate1 = row1 * column0;
+    intermediate2 = row2 * column0;
+    intermediate3 = row3 * column0;
+    _MM_TRANSPOSE4_PS(intermediate0, intermediate1, intermediate2, intermediate3);
+    destination0 += intermediate0 + intermediate1 + intermediate2 + intermediate3;
+    _mm_store_ps(C + 0, destination0);
+
+    // Compute the sums for the second column
+    __m128 column1      = _mm_load_ps(B + 4);
+    __m128 destination1 = _mm_load_ps(C + 4);
+    intermediate0 = row0 * column1;
+    intermediate1 = row1 * column1;
+    intermediate2 = row2 * column1;
+    intermediate3 = row3 * column1;
+    _MM_TRANSPOSE4_PS(intermediate0, intermediate1, intermediate2, intermediate3);
+    destination1 += intermediate0 + intermediate1 + intermediate2 + intermediate3;
+    _mm_store_ps(C + 4, destination1);
+
+    // Compute the sums for the third column
+    __m128 column2      = _mm_load_ps(B + 8);
+    __m128 destination2 = _mm_load_ps(C + 8);
+    intermediate0 = row0 * column2;
+    intermediate1 = row1 * column2;
+    intermediate2 = row2 * column2;
+    intermediate3 = row3 * column2;
+    _MM_TRANSPOSE4_PS(intermediate0, intermediate1, intermediate2, intermediate3);
+    destination2 += intermediate0 + intermediate1 + intermediate2 + intermediate3;
+    _mm_store_ps(C + 8, destination2);
+
+    // Compute the sums for the fourth column
+    __m128 column3      = _mm_load_ps(B + 12);
+    __m128 destination3 = _mm_load_ps(C + 12);
+    intermediate0 = row0 * column3;
+    intermediate1 = row1 * column3;
+    intermediate2 = row2 * column3;
+    intermediate3 = row3 * column3;
+    _MM_TRANSPOSE4_PS(intermediate0, intermediate1, intermediate2, intermediate3);
+    destination3 += intermediate0 + intermediate1 + intermediate2 + intermediate3;
+    _mm_store_ps(C + 12, destination3);
+}
 
 /* method provided by the code */
 void sgemm_reference( int m, int n, float *A, float *C)
 {
-  #pragma omp parallel for
-  for( int i = 0; i < m; i++ )
-    for( int k = 0; k < n; k++ )
-      for( int j = 0; j < m; j++ )
-        C[i+j*m] += A[i+k*m] * A[j+k*m];
-}
-
-// Transpose a matrix (respecting the padding)
-inline void Transpose(float * A, float * B, int m, int n, int bmPadded, int bnPadded)
-{
-  for(size_t i = 0; i < m; i++)
-    for(size_t j = 0; j < n; j++)
-      B[(i*bnPadded) + j] = A[i + (j*m)];
-}
-
-// Transpose a matrix
-inline void Transpose(float * A, float * B, int m, int n)
-{
-  for(size_t i = 0; i < m; i++)
-    for(size_t j = 0; j < n; j++)
-      B[(i*n) + j] = A[i + (j*m)];
-}
-
-// Computes C = A * transpose(A).  m and n must be multiples of 4. A is row major, C is column major
-inline void atimestransposea( int m, int n, float *A, float *C )
-{
-    // Iterate through the columns of the matrix
-    for(size_t i = 0; i < m; i++)
-    {
-        // Iterate through the rows of the matrix
-        for(size_t j = 0; j < m; j += 4)
-        {
-            // Summation register for these elements
-            __m128 result = _mm_set1_ps(0.0f);
-
-            // Iterate over the elements in a row
-            for(size_t k = 0; k < n; k += 4)
-            {
-                // Load r-matrix column
-                __m128 aColumn = _mm_load_ps(A + (i*n) + k);
-
-                // Load l-matrix rows
-                __m128 aRow0 = _mm_load_ps(A + ((j+0)*n) + k);
-                __m128 aRow1 = _mm_load_ps(A + ((j+1)*n) + k);
-                __m128 aRow2 = _mm_load_ps(A + ((j+2)*n) + k);
-                __m128 aRow3 = _mm_load_ps(A + ((j+3)*n) + k);
-
-                // Multiply each row by the column
-                aRow0 = aRow0 * aColumn;
-                aRow1 = aRow1 * aColumn;
-                aRow2 = aRow2 * aColumn;
-                aRow3 = aRow3 * aColumn;
-
-                // Transpose the row so that we can use vector add
-                _MM_TRANSPOSE4_PS(aRow0, aRow1, aRow2, aRow3);
-
-                // Store the summations
-                result += aRow0;
-                result += aRow1;
-                result += aRow2;
-                result += aRow3;
-            }
-
-            // Store result
-            _mm_store_ps(C + (i*m) + j, result);
-        }
-    }
+    #pragma omp parallel for
+    for( int i = 0; i < m; i++ )
+        for( int k = 0; k < n; k++ )
+            for( int j = 0; j < m; j++ )
+                C[i+j*m] += A[i+k*m] * A[j+k*m];
 }
 
 // Multiply A matrix by its transpose
 extern "C" void sgemm( int m, int n, float *A, float *C )
 {
-    // Recompute boundaries of the matrix (align to 4, for sse)
+    // Compute dimensions of "matrix of blocks"
+    int mBlocks = (m / 4) + ((m & 0x03) ? 1 : 0);
+    int nBlocks = (n / 4) + ((n & 0x03) ? 1 : 0);
+
+    // might be needed
     int mPadded = (m & ~0x03) + ((m & 0x03) ? 4 : 0);
     int nPadded = (n & ~0x03) + ((n & 0x03) ? 4 : 0);
 
-    // Compute the transpose of the matrix
-    float *At = (float *) malloc (sizeof(float) * mPadded * nPadded);
-    memset((void *) At, 0, sizeof(float) * mPadded * nPadded);
-    Transpose(A, At, m, n, mPadded, nPadded);
+    // Allocate a pointer arrays to perform block allocation in
+    float **source_blocks = (float **) calloc (mBlocks * nBlocks, sizeof(float *));
+    float **transpose_blocks = (float **) calloc (mBlocks * nBlocks, sizeof(float *));
+    float **destination_blocks = (float **) calloc (mBlocks * mBlocks, sizeof(float *));
 
-    // The matrix does not have favorable dimensions
-    if((m % 4) || (n % 4))
+    // Perform destination matrix allocation
+    for(int i = 0; i < mBlocks * mBlocks; i++)
+        destination_blocks[i] = (float *) calloc (16, sizeof(float));
+
+    // Perform source matrix allocation
+    std::cout << "------- SOURCE MATRIX ---------" << std::endl;
+    for(int i = 0; i < mBlocks; i++)   // i = row
     {
-        // Allocate new, padded matrices
-        float *Cpadded = (float *) malloc (sizeof(float) * mPadded * mPadded);
-        memset((void *) Cpadded, 0, sizeof(float) * mPadded * mPadded);
+        for(int j = 0; j < nBlocks; j++) // j = column
+        {
+            // Allocate memory for the block
+            float *block = (float *) calloc (16, sizeof(float));
+            source_blocks[ROW_MAJOR(i,j,mBlocks,nBlocks)] = block;
 
-        // Perform multiplication
-        atimestransposea(mPadded, nPadded, At, Cpadded);
+            // Compute the dimensions of this block
+            int mLocalBlock = MIN(m - (4 * i), 4);
+            int nLocalBlock = MIN(n - (4 * j), 4);
 
-        // Perform a copy of Cpadded into C matrix (optimized for column major matrices)
-        for(int j = 0; j < m; j++)
-            memcpy((void *) (C + (m*j)), (void *) (Cpadded + (mPadded*j)), sizeof(float) * m);
+            // Copy and transpose, ii = row, jj = column
+            for(int ii = 0; ii < mLocalBlock; ii++)
+                for(int jj = 0; jj < nLocalBlock; jj++)
+                {
+                    int bIdx = ROW_MAJOR(ii,jj,4,4);
+                    int sIdx = COLUMN_MAJOR((i*4)+ii,(j*4)+jj,m,n);
+                    block[bIdx] = A[sIdx];
+                }
 
-        // Cleanup
-        free(Cpadded);
+            // DEBUG!!!
+            std::cout << "Local block size: " << mLocalBlock << "," << nLocalBlock << std::endl;
+            std::stringstream name;
+            name << "A(" << i << "," << j << "):" << std::ends;
+            print_rowmajor(block, 4, 4, name.str());
+            std::cout << std::endl;
+        }
     }
+    std::cout << "-------------------------------" << std::endl << std::endl;
 
-    // Otherwise, this is an optimal case where padding is not required
-    else
+    // Perform source matrix allocation
+    /*std::cout << "------- TRANSPOSE MATRIX ---------" << std::endl;
+    for(int i = 0; i < nBlocks; i++)   // i = row
     {
-        atimestransposea(m, n, At, C);
-    }
+        for(int j = 0; j < mBlocks; j++) // j = column
+        {
+            // Allocate memory for the block
+            float *block = (float *) calloc (16, sizeof(float));
+            transpose_blocks[ROW_MAJOR(i,j,nBlocks,mBlocks)] = block;
 
-    // Release the transpose matrix
-    free(At);
+            // Compute the dimensions of this block
+            int mLocalBlock = MIN(n - (4 * i), 4);
+            int nLocalBlock = MIN(m - (4 * j), 4);
+
+            // Copy and transpose, ii = row, jj = column
+            for(int ii = 0; ii < mLocalBlock; ii++)
+                for(int jj = 0; jj < nLocalBlock; jj++)
+                {
+                    int bIdx = ROW_MAJOR(ii,jj,4,4);
+                    int sIdx = COLUMN_MAJOR((i*4)+ii,(j*4)+jj,m,n);
+                    block[bIdx] = A[sIdx];
+                }
+
+            // DEBUG!!!
+            std::cout << "Local block size: " << mLocalBlock << "," << nLocalBlock << std::endl;
+            std::stringstream name;
+            name << "A(" << i << "," << j << "):" << std::ends;
+            print_rowmajor(block, 4, 4, name.str());
+            std::cout << std::endl;
+        }
+    }
+    std::cout << "-------------------------------" << std::endl;
+
+    // Perform multiplication (i = row, j = column, k = element)
+    for(int i = 0; i < mBlocks; i++)
+    {
+        for(int j = 0; j < mBlocks; j++)
+        {
+            for(int k = 0; k < nBlocks; k++)
+            {
+                // Compute the addresses
+                int sIdx = ROW_MAJOR(i,k,mBlocks,nBlocks);
+                int tIdx = ROW_MAJOR(k,j,nBlocks,mBlocks);
+                int dIdx = COLUMN_MAJOR(i,j,mBlocks,mBlocks);
+
+                // Perform multiply and accumulate
+                block_multiply_accumulate(source_blocks[sIdx], transpose_blocks[tIdx], destination_blocks[dIdx]);
+            }
+        }
+    }*/
 }
 
 // Test
 int main ()
 {
-  size_t m = 8;
-  size_t n = 4;
-  float  matrixA[] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f,
+  size_t m = 5;
+  size_t n = 5;
+  /*float  matrixA[] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f,
                       9.0f, 10.0f, 11.0f, 12.0f, 13.0f, 14.0f, 15.0f, 16.0f,
                       1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f,
-                      9.0f, 10.0f, 11.0f, 12.0f, 13.0f, 14.0f, 15.0f, 16.0f};
+                      9.0f, 10.0f, 11.0f, 12.0f, 13.0f, 14.0f, 15.0f, 16.0f};*/
+  float  matrixA[] = {1.0f, 2.0f, 3.0f, 4.0f, 69.f,
+                      5.0f, 6.0f, 7.0f, 8.0f, 69.f,
+                      9.0f, 10.0f, 11.0f, 12.0f, 69.f,
+                      13.0f, 14.0f, 15.0f, 16.0f, 69.f,
+                      69.f, 69.f, 69.f, 69.f, 69.f};
   float *A  = (float *) matrixA;
-  float  matrixAt[m*n];
-  float *At = (float *) matrixAt;
   float  matrixC[m*m];
   float *C  = (float *) matrixC;
 
   // Print the source matrix
   std::cout << "Computing C = A * transpose(A)" << std::endl;
-  std::cout << "A []:\t";
-  for(size_t i = 0; i < m * n; i++)
-    std::cout << matrixA[i] << "\t";
-  std::cout << std::endl;
+  print_columnmajor(A, m, n, "A []:");
 
   // Compute and print the transpose matrix
-  Transpose(A, At, m, n, m, n);
-  std::cout << "A'[]:\t";
-  for(size_t i = 0; i < m * n; i++)
-    std::cout << At[i] << "\t";
-  std::cout << std::endl;
+  //Transpose(A, At, m, n, m, n);
+  //print_columnmajor(At, n, m, "A'[]:");
 
   // Perform multiplication with the original method
   memset((void *) C, 0, sizeof(float) * m * m);
   sgemm_reference(m, n, A, C);
-  std::cout << "C []:\t";
-  for(size_t i = 0; i < m * m; i++)
-    std::cout << C[i] << "\t";
-  std::cout << std::endl << std::endl;
+  print_columnmajor(C, m, m, "C []:");
+  std::cout << std::endl;
 
   // Zero the destination array
   memset((void *) C, 0, sizeof(float) * m * m);
 
   // Iterate through the columns of the matrix
   sgemm(m,n,A,C);
+  //block_multiply_accumulate(At, At, C);
 
   // Output resulting arrays
-  std::cout << "Result using SSE optimized algorithm" << std::endl;
-  std::cout << "C []:\t";
-  for(size_t i = 0; i < m * m; i++)
-    std::cout << C[i] << "\t";
-  std::cout << std::endl << std::endl;
+  //std::cout << "Result using SSE optimized algorithm" << std::endl;
+  //print_columnmajor(C, m, m, "C []:");
+  std::cout << std::endl;
 
   return 0;
 }
